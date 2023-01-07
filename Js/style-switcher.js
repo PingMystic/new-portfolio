4f9da889c3bf521c2975f814b18a2dde00b7b23a
tw9str <i88b@hotmail.com>
2023-01-07 05:52:10 +0100

    added localStorage

--- Js/style-switcher.js
+++ Js/style-switcher.js
@@ -3,14 +3,14 @@
 const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
 styleSwitcherToggle.addEventListener("click", () => {
   document.querySelector(".style-switcher").classList.toggle("open");
-})
+});
 
 // hide style - switcher on scroll
 window.addEventListener("scroll", () => {
   if (document.querySelector(".style-switcher").classList.contains("open")) {
-    document.querySelector(".style-switcher").classList.remove("open")
+    document.querySelector(".style-switcher").classList.remove("open");
   }
-})
+});
 
 /* ================== theme colors ================== */
 
@@ -20,10 +20,17 @@ function setActiveStyle(color) {
     if (color === style.getAttribute("title")) {
       style.removeAttribute("disabled");
     } else {
-      style.setAttribute("disabled","true");
+      style.setAttribute("disabled", "true");
     }
-  })
+    localStorage.setItem("color", color);
+  });
 }
+
+alternateStyles.forEach((style) => {
+  if (localStorage.color === style.getAttribute("title")) {
+    style.removeAttribute("disabled");
+  }
+});
 
 /* ================== light and dark mode ================== */
 const dayNight = document.querySelector(".day-night");
@@ -31,11 +38,21 @@ dayNight.addEventListener("click", () => {
   dayNight.querySelector("i").classList.toggle("fa-sun");
   dayNight.querySelector("i").classList.toggle("fa-moon");
   document.body.classList.toggle("dark");
-})
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.removeItem("theme");
+  }
+});
+
 window.addEventListener("load", () => {
   if (document.body.classList.contains("dark")) {
     dayNight.querySelector("i").classList.add("fa-sun");
   } else {
     dayNight.querySelector("i").classList.add("fa-moon");
   }
-})
+});
+
+if (localStorage.theme) {
+  document.body.className = localStorage.theme;
+}
